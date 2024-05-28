93396b29604c3278f65f01dd1dee1eb4ee43636f
jhantu626 <jhantubala626@gmail.com>
2024-05-29 00:48:33 +0530

    Basic configuration from pritam branch

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const app=express();
+require('dotenv').config();
 
 
-
-app.get('/',(req,res)=>{
-    res.send('hello world')
+app.get('/welcome',(req,resp)=>{
+    resp.send('welcome to our Job Portal!');
 })
 
-app.listen(8000,()=>{
-    console.log('listening on port 3000')
+const port=process.env.PORT || 8000
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
 })
