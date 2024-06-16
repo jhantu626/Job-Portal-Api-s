0ec1cbe4dcbaaf6cdf9f44f23004ecb6c3177ce6
jhantu626 <jhantubala626@gmail.com>
2024-06-16 16:37:19 +0530

    done added sorting and filtering jobs

--- controller/JobsController.js
+++ controller/JobsController.js
@@ -19,13 +19,45 @@ const createJob=async(req,resp,next)=>{
 }
 
 const getAllJobs=async(req,resp,next)=>{
-    try{
-        const jobs=await Job.find();
+    try {
+        const { status, workType, workLocation, sortBy } = req.query;
+
+        // Conditions for Searching
+        const queryObject = {};
+        if (status) {
+            queryObject.status = status;
+        }
+        if (workType) {
+            queryObject.workType = workType;
+        }
+        if (workLocation) {
+            queryObject.workLocation = workLocation;
+        }
+
+        let result = Job.find(queryObject);
+
+        // Sorting
+        if (sortBy === "Latest") {
+            result = result.sort("-createdAt");
+        }else if(sortBy=="Oldest"){
+            result=result.sort("createdAt");
+        }
+
+        // Pagination
+        const page= Number(req.query.page) || 1
+        const limit=20;
+        const startIndex=(page-1)*limit;
+        result=result.skip(startIndex).limit(limit);
+        const totalJobs=await Job.countDocuments(queryObject);
+        const totalPages=Math.ceil(totalJobs/limit);
+
+        const jobs = await result;
         resp.status(200).json({
-            totalJobs: jobs.length,
+            totalJobs: totalJobs,
+            totalPage: totalPages,
             jobs
         });
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 }
